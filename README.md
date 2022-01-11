c22d0d03e40b878dea6bb94ffc0a3ef2ec0adcb8
tzhiy <2570986081@qq.com>
2022-01-11 15:57:32 +0800

    完成所有功能

--- README.md
+++ README.md
@@ -1,24 +1,201 @@
-# red-envelope
+# 「 春节游戏」过年的压岁钱，我真的不能收！
 
-## Project setup
-```
-npm install
+![](https://raw.githubusercontent.com/tzhiy/image-repo/main/blog/image-20220111145725922.png?token=APUDWWSQSD4ARMWXWHB7I33B3U3ZM)
+
+>在线体验：[https://red-envelope.web.cloudendpoint.cn/](https://red-envelope.web.cloudendpoint.cn/)
+>
+>Github 地址：[https://github.com/tzhiy/Red-Envelope](https://github.com/tzhiy/Red-Envelope)
+
+## 游戏背景
+
+快过年了，大家在拜年的时候，压岁钱的习俗必不可少（虽然我已经过了领压岁钱的年龄）。每年在收到压岁钱的时候，我们都会推搡一下，可总是害怕推回去了。所以领压岁钱是一个技术活，这个游戏就模拟了领压岁钱的玩法，让我们在欲迎还拒的过程中最后领到压岁钱。
+
+## 游戏玩法
+
+在游戏中，我们需要扮演孩子的角色来应对亲戚的压岁钱。
+
+![](https://raw.githubusercontent.com/tzhiy/image-repo/main/blog/image-20220111150543006.png?token=APUDWWWXTAQ4LFEDF2TGCJLB3U34I)
+
+游戏开始后，屏幕下方的进度条会向左做随机速度的移动，我们每次点击屏幕，指针就会向右移动一段固定距离。
+
+倒计时结束时，如果指针停留在有效区域，你就能获得红包了！
+
+![](https://raw.githubusercontent.com/tzhiy/image-repo/main/blog/image-20220111150953725.png?token=APUDWWWXRLAHMKTQKPWJAQLB3U4AG)
+
+如果指针在游戏过程中碰到进度条的边缘，那么游戏就会失败，压岁钱会缓缓离你而去。
+
+![](https://raw.githubusercontent.com/tzhiy/image-repo/main/blog/image-20220111154632340.png?token=APUDWWS6KRDAYH2BENBZO3LB3U4BA)
+
+## 代码说明
+
+代码是使用 Vue 来写的，这里主要介绍一下进度条的指针处理，手部动画的处理和对话框的样式处理。
+
+### 进度条的指针处理
+
+首先初始化进度条，为了使有效区域在进度条中间部分，使用随机数生成有效区域的起始位置并根据起始位置得到终止位置。
+
+```js
+// 初始化进度条
+const initVaildArea = () => {
+  // ...
+  // maxLength 是进度条的长度，length 是有效区域的长度
+  pointDistance = maxLength / 2;
+  length = 300;
+  validStart = Math.random() * length - length + maxLength / 2;
+  vaildArea._rawValue.style.left = validStart + "px";
+  vaildArea._rawValue.style.width = length + "px";
+};
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+玩家每次点击游戏区域，指针向右移动一段距离。
+
+```js
+// 玩家的操作
+const handleProgress = () => {
+  // 剧情结束后才能操作
+  if (plotIsOver) {
+    pointDistance += 320;
+  }
+};
 ```
 
-### Compiles and minifies for production
-```
-npm run build
+每经过一段时间的间隔，指针向左移动一段距离，为了游戏的可玩性，设计了一个随机条件，触发后指针向左移动的距离增加。当倒计时未结束时触碰到进度条两端，游戏失败；结束后指针停在有效区，游戏成功，否则失败。
+
+```js
+// 初始化指针，倒计时
+const initPointer = () => {
+  pointer._rawValue.style.left = maxLength / 2 + "px";
+  const timer = setInterval(() => {
+    time.value -= 0.01;
+    pointDistance -= 4;
+    console.log(pointDistance);
+    if (pointDistance <= 0 || pointDistance >= maxLength) {
+      isLose.value = true;
+      clearInterval(timer);
+    }
+    if (Math.random() < 0.2 && pointDistance - 6 >= 4) {
+      pointDistance -= 6;
+    }
+    pointer._rawValue.style.left = pointDistance + "px";
+    if (time.value < 0.01) {
+      if (
+        pointDistance >= validStart &&
+        pointDistance <= validStart + length
+      ) {
+        isWin.value = true;
+      } else {
+        isLose.value = true;
+      }
+      clearInterval(timer);
+    }
+  }, 10);
+};
 ```
 
-### Lints and fixes files
-```
-npm run lint
+### 两只手的处理
+
+手的出现：
+
+```js
+// 手的出现
+const initHands = () => {
+  leftHand._rawValue.style.transform = "translateX(-200px)";
+  rightHand._rawValue.style.transform = "translateX(200px)";
+};
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+当游戏失败后，亲戚角色的手会缓慢撤回，撤回之前减慢过渡时间，撤回之后恢复原来的时间：
+
+```js
+// 撤回手
+const removeHands = () => {
+  rightHand._rawValue.style.transition = "transform 2s ease-in";
+  rightHand._rawValue.style.transform = "translateX(800px)";
+  setTimeout(() => {
+    rightHand._rawValue.style.transition = "transform 0.5s ease-in";
+  }, 2000);
+};
+```
+
+手的移动距离是随机的，当游戏失败后，移动停止并撤回亲戚角色的手：
+
+```js
+const hangHands = () => {
+  let leftHandDistance = -200;
+  let rightHandDistance = 200;
+  let time = 0;
+  const timer = setInterval(() => {
+    const random = Math.random() * 100 - 50;
+    // 防止手移动到边框外
+    if (leftHandDistance + random <= 0 && rightHandDistance + random >= 0) {
+      leftHandDistance += random;
+      rightHandDistance += random;
+    }
+    leftHand._rawValue.style.transform =
+      "translateX(" + leftHandDistance + "px)";
+    rightHand._rawValue.style.transform =
+      "translateX(" + rightHandDistance + "px)";
+    time++;
+    if (time >= 60) {
+      clearInterval(timer);
+    }
+  }, 250);
+  watch(isLose, (newValue) => {
+    if (newValue) {
+      clearInterval(timer);
+      removeHands();
+    }
+  });
+};
+```
+
+### 对话框的样式
+
+![](https://raw.githubusercontent.com/tzhiy/image-repo/main/blog/image-20220111154043496.png?token=APUDWWUBRZ3KHDUWSUZKLELB3U4AM)
+
+对话框下方有一个三角部分，使用伪类实现，这里通过两个伪类的错开叠加实现边框的效果（展示的代码经过简化）：
+
+```scss
+.dialogs {
+  position: relative;
+  top: 80px;
+  left: 0;
+  right: 0;
+  height: 160px;
+  z-index: 1;
+  &__left {
+    display: inline-block;
+    position: absolute;
+    width: 240px;
+    height: 110px;
+    border-radius: 20px;
+    border: 4px solid black;
+    background-color: white;
+    left: 20px;
+  }
+  &__left::after,
+  &__left::before {
+    position: absolute;
+    width: 0;
+    height: 0;
+    content: "";
+    left: 30px;
+    top: 110px;
+    border-right: 24px solid transparent;
+    border-bottom: 16px solid transparent;
+    border-top: 16px solid black;
+    border-left: 24px solid black;
+  }
+  &__left::after {
+    top: 104px;
+    left: 34px;
+    border-top: 16px solid white;
+    border-left: 24px solid white;
+  }
+}
+```
+
+## 总结
+
+这个游戏的创意来自于《中国式家长》这款游戏，我使用 Vue 将这个小游戏实现了下来，完整的代码可以查看 [Github](https://github.com/tzhiy/Red-Envelope)，欢迎提出建议。
+
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,619 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="game-container" @click="handleProgress">
+      <div class="time">
+        <span class="time__content">
+          {{ time.toFixed(2) }}
+        </span>
+      </div>
+      <div class="dialogs">
+        <div class="dialog dialogs__left">
+          <div class="dialog__content">{{ myDialogContent }}</div>
+        </div>
+        <div class="dialog dialogs__right">
+          <div class="dialog__content">{{ relativeDialogContent }}</div>
+        </div>
+      </div>
+      <div class="hands">
+        <div class="hand hands__left" ref="leftHand" />
+        <div class="hand hands__right" ref="rightHand" />
+      </div>
+      <div class="progress">
+        <div class="progress__bar">
+          <div class="progress__pointer" ref="pointer"></div>
+          <div class="progress__vaild-area" ref="vaildArea"></div>
+        </div>
+      </div>
+      <div class="plot" v-show="isShowPlot" @click="handleHidePlot">
+        {{ plotContent }}
+      </div>
+    </div>
+    <div class="aside">
+      <div class="aside__item aside__link">
+        <a href="https://github.com/tzhiy/Red-Envelope" target="_blank"
+          >github</a
+        >
+      </div>
+      <div class="aside__item aside__link">
+        <a href="https://juejin.cn/user/871236495218264" target="_blank"
+          >掘金</a
+        >
+      </div>
+      <div class="aside__item aside__title">{{ titleContent }}</div>
+      <div
+        class="aside__item aside__help"
+        @mouseenter="() => handleShowHelp(true)"
+        @mouseleave="() => handleShowHelp(false)"
+      >
+        ?
+      </div>
+      <div class="help" v-show="isShowHelp">
+        谁不想要压岁钱呢？🥰<br />
+        游戏开始后，点击屏幕操作进度条上的指针，倒计时结束时如果指针停留在有效区域，你就能获得红包了！
+      </div>
+      <div class="aside__item aside__start" @click="handleStart">开始游戏</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch } from "vue";
+
+// 点击开始游戏，初始化所有内容
+const useStartGameEffect = (startProcess, time) => {
+  const handleStart = () => {
+    startProcess.value = true;
+    time.value = 15;
+  };
+  return { handleStart };
+};
+
+// 开始游戏流程
+const useGameProcessEffect = (
+  isShowPlot,
+  showStartPlot,
+  initHands,
+  hangHands,
+  progress,
+  isWin,
+  isLose,
+  startProcess
+) => {
+  const startGameProcess = () => {
+    showStartPlot();
+  };
+
+  const titleContent = ref("我真的不要压岁钱！");
+
+  watch(isShowPlot, (newValue) => {
+    if (newValue === false) {
+      titleContent.value = "我真的不要压岁钱！";
+      initHands();
+      setTimeout(() => {
+        hangHands();
+        progress();
+      }, 500);
+      watch(isWin, (newValue) => {
+        if (newValue) {
+          titleContent.value = "你获得了压岁钱！😋";
+          startProcess.value = false;
+        }
+      });
+      watch(isLose, (newValue) => {
+        if (newValue) {
+          titleContent.value = "你失去了压岁钱！😭";
+          startProcess.value = false;
+        }
+      });
+    }
+  });
+  return { startGameProcess, titleContent };
+};
+
+// 开始的对话框
+const useHeadPlotEffect = () => {
+  let isShowPlot = ref(false);
+  const momPlot = ref([
+    "妈妈：来，给你三舅磕个头！小时候对你可好了~你都忘了？",
+    "妈妈：来，给你二叔磕个头！开心起来啊~",
+    "妈妈：来，给你大姨磕个头！从小最亲你了~",
+    "妈妈：来，给你小舅母磕个头！可得礼貌点哈~",
+    "妈妈：来，给你舅母磕个头！小时候尿布都是她帮你换的，你不记得了~",
+  ]);
+  let plotContent = ref("内容");
+  const handleHidePlot = () => {
+    isShowPlot.value = false;
+  };
+  const showStartPlot = () => {
+    isShowPlot.value = true;
+    plotContent.value =
+      momPlot.value[Math.floor(Math.random() * momPlot.value.length)];
+  };
+  return { plotContent, handleHidePlot, showStartPlot, isShowPlot };
+};
+
+// 手的操作
+const useHandsEffect = (leftHand, rightHand, isLose) => {
+  // 手的出现
+  const initHands = () => {
+    leftHand._rawValue.style.transform = "translateX(-200px)";
+    rightHand._rawValue.style.transform = "translateX(200px)";
+  };
+  // 撤回手
+  const removeHands = () => {
+    rightHand._rawValue.style.transition = "transform 2s ease-in";
+    rightHand._rawValue.style.transform = "translateX(800px)";
+    setTimeout(() => {
+      rightHand._rawValue.style.transition = "transform 0.5s ease-in";
+    }, 2000);
+  };
+  const hangHands = () => {
+    let leftHandDistance = -200;
+    let rightHandDistance = 200;
+    let time = 0;
+    const timer = setInterval(() => {
+      const random = Math.random() * 100 - 50;
+      if (leftHandDistance + random <= 0 && rightHandDistance + random >= 0) {
+        leftHandDistance += random;
+        rightHandDistance += random;
+      }
+      leftHand._rawValue.style.transform =
+        "translateX(" + leftHandDistance + "px)";
+      rightHand._rawValue.style.transform =
+        "translateX(" + rightHandDistance + "px)";
+      time++;
+      if (time >= 60) {
+        clearInterval(timer);
+      }
+    }, 250);
+    watch(isLose, (newValue) => {
+      if (newValue) {
+        clearInterval(timer);
+        removeHands();
+      }
+    });
+  };
+  return { initHands, hangHands };
+};
+
+// 用 time 来随机对话框
+const useDialogChangeEffect = (time) => {
+  const myDialogList = ref([
+    "使不得使不得！",
+    "我都这么大了，不该收了",
+    "都不容易，这钱我怎么能要",
+    "平时帮我太多，不收不收",
+    "别这样，别这样",
+    "这样太不好意思了，不行！",
+    "心意收下了，钱就算了",
+  ]);
+  const relativeDialogList = ref([
+    "快拿着快拿着！",
+    "你不收就是看不起我！",
+    "哎，别客气，都是自己人",
+    "不用看你妈脸色，收下吧！",
+    "收下收下，应该的嘛",
+    "压岁习俗不能省！",
+  ]);
+  const myDialogContent = ref("使不得使不得！");
+  const relativeDialogContent = ref("快拿着快拿着！");
+  watch(time, () => {
+    // time 每 0.01s 改变一次
+    if (Math.random() < 0.002) {
+      const myDialogRandom = Math.floor(
+        Math.random() * myDialogList.value.length
+      );
+      myDialogContent.value = myDialogList.value[myDialogRandom];
+    }
+  });
+  watch(time, () => {
+    // time 每 0.01s 改变一次
+    if (Math.random() < 0.002) {
+      const relativeDialogRandom = Math.floor(
+        Math.random() * relativeDialogList.value.length
+      );
+      relativeDialogContent.value =
+        relativeDialogList.value[relativeDialogRandom];
+    }
+  });
+  return { myDialogContent, relativeDialogContent };
+};
+
+// 进度条的操作
+const useProgressEffect = (pointer, vaildArea, isShowPlot) => {
+  const maxLength = 968;
+  const isWin = ref(false);
+  const isLose = ref(false);
+  let pointDistance;
+  let length;
+  let validStart;
+  let time = ref(15);
+  // 初始化进度条
+  const initVaildArea = () => {
+    isWin.value = false;
+    isLose.value = false;
+    pointDistance = maxLength / 2;
+    length = 300;
+    validStart = Math.random() * length - length + maxLength / 2;
+    vaildArea._rawValue.style.left = validStart + "px";
+    vaildArea._rawValue.style.width = length + "px";
+  };
+  let plotIsOver = false;
+  watch(isShowPlot, (newValue) => {
+    if (newValue === false) {
+      plotIsOver = true;
+    }
+  });
+  // 玩家的操作
+  const handleProgress = () => {
+    // 剧情结束后才能操作
+    if (plotIsOver) {
+      pointDistance += 320;
+    }
+  };
+  // 初始化指针，倒计时
+  const initPointer = () => {
+    pointer._rawValue.style.left = maxLength / 2 + "px";
+    const timer = setInterval(() => {
+      time.value -= 0.01;
+      pointDistance -= 4;
+      console.log(pointDistance);
+      if (pointDistance <= 0 || pointDistance >= maxLength) {
+        isLose.value = true;
+        clearInterval(timer);
+      }
+      if (Math.random() < 0.2 && pointDistance - 6 >= 4) {
+        pointDistance -= 6;
+      }
+      pointer._rawValue.style.left = pointDistance + "px";
+      if (time.value < 0.01) {
+        if (
+          pointDistance >= validStart &&
+          pointDistance <= validStart + length
+        ) {
+          isWin.value = true;
+        } else {
+          isLose.value = true;
+        }
+        clearInterval(timer);
+      }
+    }, 10);
+  };
+  const progress = () => {
+    initVaildArea();
+    initPointer();
+  };
+  return { progress, isWin, isLose, handleProgress, time };
+};
+
+// 显示帮助
+const useShowHelpEffect = () => {
+  const isShowHelp = ref(false);
+  const handleShowHelp = (sign) => {
+    isShowHelp.value = sign;
+  };
+  return { isShowHelp, handleShowHelp };
+};
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const leftHand = ref(null);
+    const rightHand = ref(null);
+    const pointer = ref(null);
+    const vaildArea = ref(null);
+    let startProcess = ref(false);
+    const { isShowPlot, plotContent, handleHidePlot, showStartPlot } =
+      useHeadPlotEffect();
+    const { progress, isWin, isLose, handleProgress, time } = useProgressEffect(
+      pointer,
+      vaildArea,
+      isShowPlot
+    );
+    const { initHands, hangHands } = useHandsEffect(
+      leftHand,
+      rightHand,
+      isLose
+    );
+    const { myDialogContent, relativeDialogContent } =
+      useDialogChangeEffect(time);
+    const { isShowHelp, handleShowHelp } = useShowHelpEffect();
+    const { startGameProcess, titleContent } = useGameProcessEffect(
+      isShowPlot,
+      showStartPlot,
+      initHands,
+      hangHands,
+      progress,
+      isWin,
+      isLose,
+      startProcess
+    );
+    const { handleStart } = useStartGameEffect(startProcess, time);
+    watch(startProcess, (newValue) => {
+      if (newValue === true) {
+        startGameProcess();
+      }
+    });
+
+    return {
+      time,
+      isShowPlot,
+      handleStart,
+      plotContent,
+      showStartPlot,
+      handleHidePlot,
+      leftHand,
+      rightHand,
+      pointer,
+      vaildArea,
+      titleContent,
+      handleProgress,
+      myDialogContent,
+      relativeDialogContent,
+      isShowHelp,
+      handleShowHelp,
+    };
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang='scss'>
+body {
+  background: linear-gradient(
+    to right,
+    rgb(230, 78, 17),
+    rgb(238, 115, 0),
+    rgb(248, 112, 0)
+  );
+}
+
+.game-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background: linear-gradient(
+    to right,
+    rgb(240, 69, 1),
+    rgb(231, 209, 5),
+    rgb(248, 112, 0)
+  );
+  transform: translate(-50%, -50%);
+  width: 1100px;
+  height: 600px;
+  border: 4px solid black;
+  user-select: none;
+}
+
+.time {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 10px;
+  height: 60px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  &__content {
+    display: inline-block;
+    line-height: 60px;
+    background-color: black;
+    color: white;
+    border-radius: 20px;
+    font-family: "Comic Sans MS";
+    font-size: 24px;
+    font-weight: 700;
+    width: 100px;
+  }
+}
+
+.progress {
+  position: absolute;
+  bottom: 30px;
+  left: 0;
+  right: 0;
+  height: 40px;
+  &__bar {
+    position: absolute;
+    background-color: rgb(230, 230, 165);
+    left: 60px;
+    right: 60px;
+    height: 28px;
+    border-radius: 16px;
+    border: 6px solid rgb(243, 139, 41);
+  }
+  &__pointer {
+    position: absolute;
+    left: 484px;
+    width: 10px;
+    top: -6px;
+    height: 40px;
+    background-color: black;
+    transition: left 0.05s linear;
+    z-index: 1;
+  }
+  &__vaild-area {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 28px;
+    background-color: rgb(238, 182, 0);
+  }
+}
+
+.dialogs {
+  position: relative;
+  top: 80px;
+  left: 0;
+  right: 0;
+  height: 160px;
+  z-index: 1;
+  .dialog {
+    display: inline-block;
+    position: absolute;
+    width: 240px;
+    height: 110px;
+    border-radius: 20px;
+    border: 4px solid black;
+    background-color: white;
+    &__content {
+      position: absolute;
+      left: 20px;
+      right: 20px;
+      bottom: 21px;
+      top: 21px;
+      font-size: 24px;
+      font-weight: bold;
+      line-height: 34px;
+    }
+  }
+  &__left {
+    left: 20px;
+  }
+  &__right {
+    right: 20px;
+  }
+  .dialog::after,
+  .dialog::before {
+    position: absolute;
+    width: 0;
+    height: 0;
+    content: "";
+  }
+  &__left::after,
+  &__left::before {
+    left: 30px;
+    top: 110px;
+    border-right: 24px solid transparent;
+    border-bottom: 16px solid transparent;
+    border-top: 16px solid black;
+    border-left: 24px solid black;
+  }
+  &__left::after {
+    top: 104px;
+    left: 34px;
+    border-top: 16px solid white;
+    border-left: 24px solid white;
+  }
+  &__right::after,
+  &__right::before {
+    right: 30px;
+    top: 110px;
+    border-left: 24px solid transparent;
+    border-bottom: 16px solid transparent;
+    border-top: 16px solid black;
+    border-right: 24px solid black;
+  }
+  &__right::after {
+    top: 104px;
+    right: 34px;
+    border-top: 16px solid white;
+    border-right: 24px solid white;
+  }
+}
+
+.plot {
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  right: 20px;
+  background-color: rgb(255, 187, 109);
+  z-index: 4;
+  height: 220px;
+  border-radius: 12px;
+  border: 4px solid rgb(117, 64, 4);
+  padding: 30px 40px;
+  line-height: 40px;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.aside {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -368px);
+  height: 60px;
+  width: 1100px;
+  background-color: rgb(235, 92, 49);
+  border: 4px solid black;
+  font-weight: bold;
+  line-height: 48px;
+  font-size: 20px;
+  text-align: center;
+  user-select: none;
+  &__item {
+    border-radius: 8px;
+    margin: 2px 0;
+    border: 4px solid black;
+    background-color: rgb(255, 230, 0);
+  }
+  &__link {
+    flex: 1;
+    margin-right: 20px;
+  }
+  &__title {
+    flex: 7;
+    margin-left: 180px;
+    margin-right: 10px;
+    font-family: "Comic Sans MS";
+  }
+  &__help {
+    flex: 1;
+    margin-right: 20px;
+  }
+  &__start {
+    flex: 4;
+  }
+}
+
+.hands {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 80px;
+  height: 400px;
+  overflow: hidden;
+  .hand {
+    position: absolute;
+    width: 800px;
+    height: 360px;
+    bottom: 20px;
+    transition: transform 0.5s ease-in;
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  &__left {
+    left: 0;
+    transform: translateX(-800px);
+    background-image: url("./assets/lefthand.png");
+    background-position: right;
+    z-index: 2;
+  }
+  &__right {
+    right: 0;
+    transform: translateX(800px);
+    background-image: url("./assets/righthand.png");
+    background-position: left;
+    z-index: 3;
+  }
+}
+
+.help {
+  position: absolute;
+  width: 300px;
+  left: 660px;
+  top: 60px;
+  background-color: rgb(255, 243, 136);
+  text-align: left;
+  font-size: 18px;
+  border-radius: 12px;
+  border: 2px solid black;
+  box-sizing: border-box;
+  padding: 4px 10px;
+}
+
+a {
+  text-decoration: none;
+  color: black;
 }
 </style>
